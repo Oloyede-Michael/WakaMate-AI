5b270e449369f5c0ac13e158881301a8e476c274
Kars07 <eniaiyejuni.kayode@gmail.com>
2025-08-02 13:58:16 +0100

    Agents Added

---
+++ wakamate_deliver_route/src/wakamate_deliver_route/register.py
@@ -0,0 +1,5 @@
+# pylint: disable=unused-import
+# flake8: noqa
+
+# Import any tools which need to be automatically registered here
+from wakamate_deliver_route import wakamate_deliver_route_function
